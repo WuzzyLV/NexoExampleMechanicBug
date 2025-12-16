5e0af1668d647015741274965dd0b8b6f69635e0
WuzzyLV <WuzzyLV@users.noreply.github.com>
2025-12-17 01:40:49 +0200

    delete kotlin project

--- src/main/kotlin/com/nexomc/mechanic_example/ExampleMechanic.kt
+++
@@ -1,9 +0,0 @@
-package com.nexomc.mechanic_example
-
-import com.nexomc.nexo.mechanics.Mechanic
-import org.bukkit.configuration.ConfigurationSection
-
-class ExampleMechanic(factory: ExampleMechanicFactory, section: ConfigurationSection) : Mechanic(factory, section) {
-    val something: String = section.getString("something") ?: "nothing"
-    val bool: Boolean = section.getBoolean("bool")
-}
